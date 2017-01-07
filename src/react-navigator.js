553253100ac947204e0974d0e0dc752d407e600e
chaixb <chaixiaobing7@gmail.com>
2017-01-07 23:42:52 +0800

    update

--- src/react-navigator.js
+++ src/react-navigator.js
@@ -27,12 +27,13 @@ class Link extends React.Component {
 
   }
 
-  onClick() {
-    // this.props.onClick && this.props.onClick();
+  onClick(e) {
+    e.stopPropagation();
+    this.props.onClick && this.props.onClick(e.target);
   }
 
   render () {
-    return <a ref={ ele => this.ele = ele } onClick={this.onClick}className="link" href="#">{this.props.name}</a>
+    return <a ref={ ele => this.ele = ele } onClick={this.onClick.bind(this)}className="link" href="#">{this.props.name}</a>
   }
 }
 
@@ -45,9 +46,22 @@ class Link extends React.Component {
 
 
 class LinkGroup extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      translateX: 0,
+      $wrapper: null,
+      $ele: null
+    }
+  }
+
+
 
   componentDidMount () {
     const { $wrapper, $ele } = this.refs
+
+    this.state.$wrapper = $wrapper;
+    this.state.$ele     = $ele;
 
     const eleWidth     = $ele.offsetWidth;
     const wrapperWidth = $wrapper.offsetWidth;
@@ -55,11 +69,6 @@ class LinkGroup extends React.Component {
 
     var start      = { x: 0, y: 0 };
     var end        = { x: 0, y: 0 };
-    var translateX = 0;
-
-    function onupdate () {
-
-    }
 
     $ele.addEventListener('touchstart', (e) => {
       const touch = e.touches[0];
@@ -70,33 +79,62 @@ class LinkGroup extends React.Component {
     $ele.addEventListener('touchmove', (e) => {
       end.x = e.touches[0].pageX;
       const deltaX = end.x - start.x;
-      setTranslateX($wrapper, translateX + deltaX);
+      setTranslateX($wrapper, this.state.translateX + deltaX);
     })
 
+    // 如果左右超出，产生一个动画回弹
     $ele.addEventListener('touchend', (e) => {
-      translateX += end.x - start.x;
-      // 如果左右超出，产生一个动画回弹
-      var o = { x: translateX };
+      const deltaX = end.x - start.x
+      this.state.translateX += deltaX;
+
       var x = null;
 
-      if (translateX >= 0) { x = 0; }
-      else if (translateX <= maxDelta) { x = maxDelta; }
+      if (this.state.translateX >= 0) { x = 0; }
+      else if (this.state.translateX <= maxDelta) { x = maxDelta; }
 
-      if (x != null) {
-        Tween.to(o, 0.5, { x: x, onUpdate: function() {
-          setTranslateX($wrapper, o.x);
-        }, onComplete: function () {
-          translateX = x;
-        }})
+      if (x != null && deltaX != 0) {
+        this.updateTranslateX(x, 0.5)
       }
 
       start.x = end.x;
     })
   }
 
+  updateTranslateX (x, timeout) {
+    var o = { x: this.state.translateX };
+    Tween.to(o, timeout, { x: x, onUpdate: () => {
+      setTranslateX(this.state.$wrapper, o.x);
+    }, onComplete:  () => {
+      this.state.translateX = x;
+    }})
+  }
+
+  onClick ($target) {
+    const wrapperRect = this.state.$wrapper.getBoundingClientRect()
+    const targetRect  = $target.getBoundingClientRect()
+
+    const wrapperLeft = wrapperRect.left;
+    const targetLeft  = targetRect.left;
+
+    const disToEleMid = targetLeft - wrapperLeft + $target.offsetWidth / 2 + this.state.translateX - this.state.$ele.offsetWidth / 2;
+
+    var result = null;
+
+    if (disToEleMid >= 0) {
+      let t = this.state.translateX + this.state.$wrapper.offsetWidth - this.state.$ele.offsetWidth
+      result = Math.min(t, disToEleMid)
+    } else if (disToEleMid < 0) {
+      result = Math.max(disToEleMid, this.state.translateX)
+    }
+
+    console.log(result);
+    this.updateTranslateX(this.state.translateX - result, 0.2)
+
+  }
+
   render () {
     const result = this.props.links.map((link, i) => {
-      return <Link name={link.name} key={i}></Link>
+      return <Link name={link.name} key={i} onClick={this.onClick.bind(this)}></Link>
     })
 
     return (
